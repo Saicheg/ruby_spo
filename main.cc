ffc9a191245586b4c5be9edd4237c52163a78127
Valentine Zavadsky <saicheg@gmail.com>
2012-05-27 18:33:37 +0300

    HOLY F******G BISON\!

---
+++ main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bobcat/readlinestream>
+
+#include "Scanner.h"
+#include "Parser.h"
+
+using namespace std;
+using namespace FBB;
+
+int main()
+{
+    Parser parser;
+    parser.parse();
+}
