e2cdee20c5745248e428338244a4a18e2eb37ee3
Valentine Zavadsky <saicheg@gmail.com>
2012-05-30 21:50:38 +0300

    new create_token utility method

--- utils.h
+++ utils.h
@@ -22,4 +22,33 @@ long int s2int( const string& s)
 	return x;
 } 
 
+SyntaxToken* CreateOperationToken(string operationType, SyntaxToken* &arg1, SyntaxToken* &arg2)
+{
+    SyntaxToken* opType = new StringSyntaxToken(operationType);
+    opType->SetType(SyntaxTokenType::OperationTypeToken);
+    
+    SyntaxToken* t = new SyntaxToken(SyntaxTokenType::OperationToken);
+    
+    t->Children().push_back(*opType);
+
+    if(arg1 == NULL)
+    {
+    	return NULL;	
+    }
+
+    t->Children().push_back(*arg1);
+
+    if(arg2 != NULL)
+    {
+    	t->Children().push_back(*arg2);
+	    delete arg2;
+    }
+
+    delete opType;
+    delete arg1;
+
+    return t;
+
+}
+
 #endif
